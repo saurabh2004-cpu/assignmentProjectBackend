8ab1ef5ab00dfab6b77c21870a3cb0fbc9db683e
Your Name <saurabhbodakhe2004@gmail.com>
2024-12-10 15:25:34 +0530

    resolve error

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -136,6 +136,7 @@ const logoutUser = async (req, res) => {
         const options = {
             httpOnly: true,
             secure: true,
+            sameSite: 'None'
         }
         
         return res
